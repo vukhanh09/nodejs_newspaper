833bb507738c9311fb9140243ad919bd4bdd9f14
toquq <tuannha@corp.sunivy.com>
2022-01-16 22:21:58 +0700

    continue merge api

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -15,6 +15,7 @@ module.exports = function(app) {
     app.get("/api/news/get-hot-news-by-topic", controller.getHotNewsByTopic);
     app.get("/api/news/get-top10-hot-news", controller.getTop10News);
     app.get("/api/news/get-3-newest-news", controller.get3LastAddedNews);
+    app.get("/api/news/get-3-newest-news-topic", controller.get3LastAddedNewsOfTopic);
     app.get("/api/news/get-top3-news-topic", controller.getTop3NewsOfTopic);
     app.post("/api/news/add-news", controller.addNews);
     app.get("/api/news/get-all-news", controller.getAllNews);
